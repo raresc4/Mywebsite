93db1e427e5404b39e91d32a388b8defa731c488
raresc4 <rares.catana@gmail.com>
2024-03-04 13:22:24 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Component1} from "./Component1"
+import rares from "./instabig.jpg"
 
 function App() {
   return (
     <div>
-
       <Component1 />
     </div>
   )   
--- src/Component1.jsx
+++ src/Component1.jsx
@@ -8,25 +8,27 @@ export class Component1 extends Component{
     render(){
         return (
             <div className="Principal">
-                <span className="Imagine">
-                    <img src={imagine} alt="rarescatana"/>
-                </span>
-                <h1 className="Header1">RARES CATANA</h1>
-                <ul className='Lista1'>
-                    <li>
-                        <FaGithub/>
-                        <a href="https://github.com/raresc4">Github</a>
-                    </li>
-                    <li>
-                        <CiLinkedin />
-                        <a href="https://www.linkedin.com/in/rare%C8%99-alexandru-catan%C4%83-652022285/">Linkedin</a>
-                    </li>
-                    <li>
-                    <FaInstagram />
-                    <a href="https://www.instagram.com/raresc4/">Instagram</a>
-                    </li>
-                </ul>
+                <div className="Text">
+                    <h1 className="Header1">RARES CATANA</h1>
+                    <ul className='Lista1'>
+                        <li>
+                            <FaGithub className="Logos"/>
+                            <a href="https://github.com/raresc4">Github</a>
+                        </li>
+                        <li>
+                            <CiLinkedin className="Logos"/>
+                            <a href="https://www.linkedin.com/in/rare%C8%99-alexandru-catan%C4%83-652022285/">Linkedin</a>
+                        </li>
+                        <li>
+                        <FaInstagram className="Logos"/>
+                        <a href="https://www.instagram.com/raresc4/">Instagram</a>
+                        </li>
+                    </ul>
                 </div>
+                <div className="Imagine">
+                    <img src={imagine} alt="rares"/>
+                </div>
+            </div>
         )
     }
 }
